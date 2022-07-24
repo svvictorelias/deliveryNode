a4f0258007f0f5509839e5355d8094390053165c
Victor Elias <svvictorelias@gmail.com>
2022-07-24 17:08:32 -0300

    Feat: available deliverys

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { AuthenticateClientController } from "./modules/account/authenticateClie
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
+import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 
@@ -14,13 +15,15 @@ const authenticateClientController = new AuthenticateClientController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 const createDeliverymanController = new CreateDeliverymanController()
 const deliveryController = new CreateDeliveryController()
+const findAllAvailableController = new FindAllAvailableController()
 
 
 routes.post("/client/", createClientController.handle)
 routes.post("/client/authenticate/", authenticateClientController.handle)
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle)
 routes.post("/deliveryman/", createDeliverymanController.handle)
-
 routes.post("/delivery", ensureAuthenticateClient, deliveryController.handle)
+routes.get("/delivery/available", findAllAvailableController.handle)
+
 
 export {routes}
